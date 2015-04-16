8078eefd605a236e2b658387f5c10cc09a4bf9c5
Bachelor 3 <bachelor3@fro-207-17.fro-207.froment.in.iesanetwork.com>
2015-04-16 14:59:46 +0200

    add exo 7

--- exo-07/hello/platforms/ios/www/js/index.js
+++ exo-07/hello/platforms/ios/www/js/index.js
@@ -20,6 +20,10 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+        console.log(navigator);
+        console.log(navigator.connection);
+        console.log(navigator.connection.type);
+        //this.checkConnection();
     },
     // Bind Event Listeners
     //
@@ -37,16 +41,22 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    },
+    checkConnection: function() {
+        var networkState = navigator.connection.type;
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.CELL]     = 'Cell generic connection';
+        states[Connection.NONE]     = 'No network connection';
 
-        console.log('Received Event: ' + id);
+        alert('Connection type: ' + states[networkState]);
     }
 };
 
 app.initialize();
-
