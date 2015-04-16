958716a7d8c7f65acc1c85d545f1e4db65dc7abe
Bachelor 3 <bachelor3@fro-207-17.fro-207.froment.in.iesanetwork.com>
2015-04-16 16:22:03 +0200

    add exo 10

--- exo-07/hello/platforms/ios/www/js/index.js
+++ exo-07/hello/platforms/ios/www/js/index.js
@@ -20,10 +20,6 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
-        console.log(navigator);
-        console.log(navigator.connection);
-        console.log(navigator.connection.type);
-        //this.checkConnection();
     },
     // Bind Event Listeners
     //
@@ -38,24 +34,16 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        var element = document.getElementById('deviceProperties');
+                element.innerHTML = 'Device Model: '    + device.model    + '<br />' +
+                                'Device Cordova: '  + device.cordova  + '<br />' +
+                                'Device Platform: ' + device.platform + '<br />' +
+                                'Device UUID: '     + device.uuid     + '<br />' +
+                                'Device Version: '  + device.version  + '<br />' +
+                                'navigator.connection.type: ' + navigator.connection.type;
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-    },
-    checkConnection: function() {
-        var networkState = navigator.connection.type;
-
-        var states = {};
-        states[Connection.UNKNOWN]  = 'Unknown connection';
-        states[Connection.ETHERNET] = 'Ethernet connection';
-        states[Connection.WIFI]     = 'WiFi connection';
-        states[Connection.CELL_2G]  = 'Cell 2G connection';
-        states[Connection.CELL_3G]  = 'Cell 3G connection';
-        states[Connection.CELL_4G]  = 'Cell 4G connection';
-        states[Connection.CELL]     = 'Cell generic connection';
-        states[Connection.NONE]     = 'No network connection';
-
-        alert('Connection type: ' + states[networkState]);
     }
 };
 
--- exo-07/hello/www/js/index.js
+++ exo-07/hello/www/js/index.js
@@ -34,6 +34,13 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        var element = document.getElementById('deviceProperties');
+                element.innerHTML = 'Device Model: '    + device.model    + '<br />' +
+                                'Device Cordova: '  + device.cordova  + '<br />' +
+                                'Device Platform: ' + device.platform + '<br />' +
+                                'Device UUID: '     + device.uuid     + '<br />' +
+                                'Device Version: '  + device.version  + '<br />' +
+                                'navigator.connection.type: ' + navigator.connection.type;
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
